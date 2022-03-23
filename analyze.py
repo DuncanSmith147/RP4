4e07385bffa1fd30fd0888ffb472551877ccabd7
DuncanSmith147 <duncan.g.smith@manchester.ac.uk>
2022-03-23 15:39:13 +0000

    Refactored analyze.py
    Replaced naive function with analysis classes. Added Pandas data frame
    output.

--- analyze.py
+++ analyze.py
@@ -20,20 +20,19 @@
 ##OTHER DEALINGS IN THE SOFTWARE.
 
 
+import os
 from collections import defaultdict
+import json
 
 import nltk
+import pandas as pd
 
 from . import extract
 from . import utils
 from . import tokenize
 
-from imp import reload
-reload(extract)
-reload(utils)
-reload(tokenize)
 
-def eval_indices(indices):
+def _eval_indices(indices):
     # "indices" is a list of lists of integers (indices).
     # returns True if it is possible to choose an index from
     # each of the lists in turn s.t. the selected indices are
@@ -54,6 +53,87 @@ def eval_indices(indices):
         minval = cand
     return True
 
+def _eval_indices2(indices):
+    # "indices" is a list of lists of integers (indices).
+    # returns the number of ways it is possible to choose an index from
+    # each of the lists in turn s.t. the selected indices are
+    # in increasing order.
+    indices = [sorted(lis) for lis in indices]
+    # go through process of constructing tree
+    # but just record leaf nodes and frequencies
+    # (numpaths) at each level
+    last_indices = indices[-1]
+    last_degrees = [1]*len(indices[-1])
+    for inds in reversed(indices[:-1]):
+        #print ('called', inds, last_indices)
+        nodes, degrees = _f(inds, last_indices)
+        if not nodes:
+            break
+        #print (nodes, degrees)
+        #print ('ld', last_degrees)
+        last_degrees = last_degrees[-degrees[0]:]
+        #print ('ld', last_degrees)
+        # accumulate last_degrees in reverse
+        for i in range(-2, -len(last_degrees)-1, -1):
+            last_degrees[i] += last_degrees[i+1]
+        #print ('ld', last_degrees)
+        last_degrees = [last_degrees[len(last_degrees) - degree] for degree in degrees]
+        #print ('ld', last_degrees)
+        last_indices = nodes
+    else:
+        return sum(last_degrees)
+    return 0
+
+def _eval_indices2_(indices):
+    # brute force approach for testing purposes
+    # checks each element in the Cartesian product
+    # for monotonicity
+    from itertools import product
+    indices = [sorted(lis) for lis in indices]
+    cnt = 0
+    for tup in product(*indices):
+        if all(v<w for v, w in zip(tup, tup[1:])):
+            cnt += 1
+    return cnt
+
+def _f(lis1, lis2):
+    # lis1 and lis2 are sorted lists of
+    # objects that support comparison
+    # returns the nodes (with non-zero outdegree)
+    # and the degrees of
+    # the bipartite graph with an edge from
+    # each node, v, in lis1 to each node, w,
+    # in lis2 s.t. v < w
+    it1 = iter(lis1)
+    it2 = enumerate(lis2)
+    nodes = []
+    degrees = []
+    v = next(it1)
+    j, w = next(it2)
+    try:
+        while True:
+            if v < w:
+                nodes.append(v)
+                degrees.append(len(lis2) - j)
+                v = next(it1)
+            else:
+                j, w = next(it2)
+    except StopIteration:
+        pass
+    return nodes, degrees
+
+def _test_eval_indices2():
+    import random
+    for _ in range(100):
+        data = [random.choices(range(30), k=6),
+                random.choices(range(40), k=6),
+                random.choices(range(50), k=6),
+                random.choices(range(60), k=6),
+                random.choices(range(70), k=6),
+                random.choices(range(80), k=6)]
+        if not _eval_indices2(data) == _eval_indices2_(data):
+            print ('Fail')
+
 def check_rule(words, rule):
     # words is a list of words from a section / paragraph / sentence
     # each word must be separated from punctuation characters e.g. ['dog', '.']
@@ -61,31 +141,162 @@ def check_rule(words, rule):
     # be careful with case (e.g. lower case all words)
     indices = [[] for _ in rule]
     for i, word_set in enumerate(rule):
-        intersect = word_set.intersection(words)
-        if intersect:
-            indices[i].extend([words.index(word) for word in intersect])
-        else:
+        for j, word in enumerate(words):
+            if word in word_set:
+                indices[i].append(j)
+        if not indices[i]:
             return False
-    return eval_indices(indices)
+    return _eval_indices(indices)
 
-def naive(filename, pipeline, rules_file):
-    # get text as single string and lower case
+def check_freq(words, rule):
+    indices = [[] for _ in rule]
+    for i, word_set in enumerate(rule):
+        for j, word in enumerate(words):
+            if word in word_set:
+                indices[i].append(j)
+        if not indices[i]:
+            return 0
+    return _eval_indices2(indices)
+
+def get_sentence_texts(filename):
+    # A convenience function that illustrates
+    # how to construct a pipeline and use it
+    # to generate sentence texts
+    # Returns a list
+    # get raw text
     text = extract.process(filename).decode().lower()
-    # apply pipeline
-    word_lists = list(pipeline(text))
-    # get rules
-    rules_data = utils.read_json_data(rules_file)
-    # for efficiency remove longer descriptions and
-    # convert lists to sets
-    rules = {}
-    for key, lis in rules_data.items():
-        rules[key] = [set(item) for item in lis[1:]]
-    # apply each rule to each list of words
-    res = defaultdict(list)
-    for key, rule in rules.items():
-        for word_list in word_lists:
-            if check_rule(word_list, rule):
-                res[key].append(word_list)
-    return res
+    # construct pipeline
+    pipeline = utils.Pipeline([nltk.sent_tokenize, tokenize.word_tokenize])
+    filter1 = utils.replacement_factory(utils.read_json_data('synonyms.dat'))
+    filter2 = utils.replacement_factory(utils.read_json_data('hyponyms.dat'))
+    pipeline.extend([filter1, filter2])
+    # process raw data and return
+    # list of texts
+    return list(pipeline(text))
+
+def get_paragraph_texts(filename):
+    # A convenience function that illustrates
+    # how to construct a pipeline and use it
+    # to generate paragraph texts
+    # Returns a list
+    # get raw text
+    text = extract.process(filename).decode().lower()
+    # construct pipeline
+    pipeline = utils.Pipeline([tokenize.paragraph_tokenize, tokenize.word_tokenize])
+    filter1 = utils.replacement_factory(utils.read_json_data('synonyms.dat'))
+    filter2 = utils.replacement_factory(utils.read_json_data('hyponyms.dat'))
+    pipeline.extend([filter1, filter2])
+    # process raw data and return
+    # list of texts
+    return list(pipeline(text))
+
+
+class Analyzer:
+    def __init__(self, rules):
+        # if rules is a filepath load
+        # rules (assumed to be in json format)
+        if isinstance(rules, (str, bytes, os.PathLike)):
+            self.rules = utils.read_json_data(rules)
+        else:
+            self.rules = rules
+
+    def analyze(self, texts):
+        raise NotImplementedError
+
+
+class BoolAnalyzer(Analyzer):
+    def __init__(self, rules):
+        Analyzer.__init__(self, rules)
+        # for efficiency remove longer descriptions and
+        # convert lists to sets
+        for key, lis in list(self.rules.items()):
+            self.rules[key] = [set(item) for item in lis[1:]]
+        self.texts = None
+        self.results = None
+
+    def analyze(self, texts):
+        # list of lists of words
+        res = defaultdict(list)
+        for i, text in enumerate(texts):
+            for key, rule in self.rules.items():
+                if check_rule(text, rule):
+                    res[key].append(i)
+        self.texts, self.results = texts, res
+
+    def table_output(self):
+        # returns array with rows corresponding
+        # to indices of texts and columns corresponding
+        # to rule keys (in sorted order)
+        # Returns a sparse matrix (dict mapping coordinates
+        # to non-zero values) with text indicess as row indices
+        # and sorted feature keys as columns
+        texts, res = self.texts, self.results
+        keys = sorted(self.rules.keys())
+        arr = {}
+        for j, key in enumerate(keys):
+            for i in res[key]:
+                arr[(i,j)] = 1
+        return arr
+
+    def data_frame(self):
+        texts, res = self.texts, self.results
+        keys = sorted(self.rules.keys())
+        # create dictionary of sparse arrays with
+        # insertion order equal to order of keys (i.e. sorted)
+        d = {}
+        for key in keys:
+            lis = [0]*len(texts)
+            for i in res[key]:
+                lis[i] = 1
+            d[key] = pd.arrays.SparseArray(lis, dtype='int32')
+        return pd.DataFrame(d, columns=keys)
+
+
+class FreqAnalyzer(BoolAnalyzer):
+    # uses same rules so inherit from BoolAnalyzer
+    def __init__(self, rules):
+        BoolAnalyzer.__init__(self, rules)
+
+    def analyze(self, texts):
+        # list of lists of words
+        res = defaultdict(list)
+        for i, text in enumerate(texts):
+            for key, rule in self.rules.items():
+                freq = check_freq(text, rule)
+                if freq:
+                    res[key].append((i, freq))
+        self.texts, self.results = texts, res
+
+    def table_output(self):
+        # returns array with rows corresponding
+        # to indices of texts and columns corresponding
+        # to rule keys (in sorted order)
+        # Returns a sparse matrix (dict mapping coordinates
+        # to non-zero values) with text indicess as row indices
+        # and sorted feature keys as columns
+        texts, res = self.texts, self.results
+        keys = sorted(self.rules.keys())
+        arr = {}
+        for j, key in enumerate(keys):
+            for (i, freq) in res[key]:
+                arr[(i,j)] = freq
+        return arr
+
+    def data_frame(self):
+        texts, res = self.texts, self.results
+        keys = sorted(self.rules.keys())
+        # create dictionary of sparse arrays with
+        # insertion order equal to order of keys (i.e. sorted)
+        d = {}
+        for key in keys:
+            lis = [0]*len(texts)
+            for i,j in res[key]:
+                lis[i] = j
+            d[key] = pd.arrays.SparseArray(lis, dtype='int32')
+        return pd.DataFrame(d, columns=keys)
+
+
+
+
 
 
